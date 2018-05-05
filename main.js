4e94083c68ba1f5c833fbfa973fc50d3e00767c8
hrmendoza96 <hrmendoza96@unitec.edu>
2018-05-05 12:57:49 -0600

    Segundo Avance

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-document.getElementById('TempInput').addEventListener('input', 
+//Celsius
+document.getElementById('Celsius').addEventListener('input', 
 function(e){
-    let tempCelsius = e.target.value;
-    document.getElementById('FahrOutput').innerHTML = (tempCelsius*(9/5))+32;
-    document.getElementById('KelvinOutput').innerHTML = (tempCelsius*1)+273.15; //se multiplica el tempCelsius por 1, ya que sin el solo acumulaba la sumatoria como que fuera un string
+    let tempCelsius = parseFloat(e.target.value);
+    console.log(tempCelsius);
+    document.getElementById('Fahr').value = (tempCelsius*(9/5))+32;
+    document.getElementById('Kelvin').value = tempCelsius+273.15; 
+});
+
+//Fahrenheit
+document.getElementById('Fahr').addEventListener('input', 
+function(e){
+    let tempFahr = parseFloat(e.target.value);
+    console.log(tempFahr);
+    document.getElementById('Celsius').value = (tempFahr-32)*(5/9);
+    document.getElementById('Kelvin').value = (tempFahr+459.67)*(5/9); 
+});
+
+//Kelvin
+document.getElementById('Kelvin').addEventListener('input', 
+function(e){
+    let tempKelvin = parseFloat(e.target.value);
+    console.log(tempKelvin);
+    document.getElementById('Fahr').value = (tempKelvin*(9/5))-459.67
+    document.getElementById('Celsius').value = tempKelvin-273.15; 
 });
